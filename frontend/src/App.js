4300089870e76638103f78f5177d23576be085a2
osekisi <osekisinde@gmail.com>
2022-07-12 17:15:48 +0200

    about us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
       <Routes>
         <Route path='/tests' element={<Navbar/>}/>
+        <Route path='/aboutus' element={<AboutUs/>}/>
       </Routes>
       </Router>
     </div>
