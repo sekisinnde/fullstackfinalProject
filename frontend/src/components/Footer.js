bff32d6f66c7bbf4b2f8188a643718724672a82a
osekisi <osekisinde@gmail.com>
2022-07-15 15:25:25 +0200

    footer updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,17 +2,9 @@ import React from "react";
 
  const Footer = () => {
   return (
-<<<<<<< HEAD
-    <Box sx={{ flexGrow: 1 }} >
-      <AppBar position="static">
-        <>blablabla</>
-      </AppBar>     
-    </Box>
-=======
     <div className='footer'>
       <p>blablabla</p>
     </div>
->>>>>>> nicolas
   );
 }
 
