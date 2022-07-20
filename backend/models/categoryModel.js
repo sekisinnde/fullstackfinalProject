c502ec605360b5d30c69b4c126fef31309034b3b
osekisi <osekisinde@gmail.com>
2022-07-20 11:58:14 +0200

    hard reset

--- backend/models/categoryModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const categorySchema = new mongoose.Schema({
-  name: String
-})
-
-const Category = mongoose.model('user', categorySchema);
-
-export default Category;
