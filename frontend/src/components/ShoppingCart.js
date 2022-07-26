3459681daf9c5925b8cb093ee4ac9d0fd12871e5
osekisi <osekisinde@gmail.com>
2022-07-26 10:38:56 +0200

    secondecommit

--- frontend/src/components/ShoppingCart.js
+++ frontend/src/components/ShoppingCart.js
@@ -1,329 +1,65 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router";
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
-// // function Copyright(props) {
-// //     return (
-// //       <Typography variant="body2" color="text.secondary" align="center" {...props}>
-// //         {'Copyright © '}
-// //         <Link color="inherit" href="https://mui.com/">
-// //           Your Website
-// //         </Link>{' '}
-// //         {new Date().getFullYear()}
-// //         {'.'}
-// //       </Typography>
-// //     );
-// //   }
+    import React from 'react'
+    import { useEffect, useState } from "react";
+    import { useParams } from 'react-router-dom';
 
-// const theme = createTheme();
+    const ShoppingCart = () => {
 
+    const itemsData = [
+    {product: "Snowball Furby", qty: 2,price:30 +'$'},
+    {product: "Bear", qty: 3,price:70 +'$'},
+    {product: "Panda", qty: 1,price:90 +'$'},
+    {product: "Bumble Bee", qty: 2,price:80 +'$'},
+    {product: "Kiwi", qty: 5,price:50 +'$'},
+    {product: "Bumble Bee", qty: 6,price:800 +'$'}
+    ];
 
-// const ShoppingCart=()=>{
-//   const [form, setForm] = useState({
-//     email:"",
-//     password:"",
-//     firstName:"",
-//     lastName:"",
-//     adress:"",
-//     adressNumber:"",
-//     isAdmin: Boolean
-//   });
-//   const navigate = useNavigate();
-  
-//   function updateForm(value) {
-//     return setForm((prev) => {
-//       return { ...prev, ...value };
-//     });
-//   }
+    let params = useParams();
+    const [category, setCategory] = useState({})
 
-//  // This function will handle the submission.
-//  async function onSubmit(e) {
-//   e.preventDefault();
+    useEffect(() => {
+    fetch(`http://localhost:5000/cart/${params.id}`)
+        .then((res) => {return res.json()})
+        .then((res) => {setCategory(res)})
+    }, [])
 
-//   // When a post request is sent to the create url, we'll add a new record to the database.
-//   const newUser = { ...form };
-//   console.log(newUser);
-
-//   await fetch("http://localhost:5000/users", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(newUser),
-//   })
-//   .catch(error => {
-//     window.alert(error);
-//     return;
-//   });
-
-//   setForm({ email: "", password: "", firstName: "", lastName:"",adress:"",adressNumber:"",isAdmin:Boolean });
-//   navigate("/");
-// }
-
-//     return(
-//     <div>
-//         <ThemeProvider theme={theme}>
-//       <Container component="main" maxWidth="xs">
-//         <CssBaseline />
-//         <Box
-//           sx={{
-//             marginTop: 8,
-//             display: 'flex',
-//             flexDirection: 'column',
-//             alignItems: 'center',
-//           }}
-//         >
-//           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-//             <LockOutlinedIcon />
-//           </Avatar>
-//           <Typography component="h1" variant="h5">
-//             Sign up
-//           </Typography>
-//           <Box component="form" noValidate  sx={{ mt: 3 }}>
-//             <Grid container spacing={2}>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   autoComplete="given-name"
-//                   name="firstName"
-//                   required
-//                   fullWidth
-//                   id="firstName"
-//                   label="First Name"
-//                   autoFocus
-//                   onChange={(e)=>updateForm({firstName: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="lastName"
-//                   label="Last Name"
-//                   name="lastName"
-//                   autoComplete="family-name"
-//                   onChange={(e)=>updateForm({lastName: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="email"
-//                   label="Email Address"
-//                   name="email"
-//                   autoComplete="email"
-//                   onChange={(e)=>updateForm({email: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={3}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="adressNumber"
-//                   label="adressNumber"
-//                   name="adressNumber"
-//                   autoComplete="adressNumber"
-//                   onChange={(e)=>updateForm({adressNumber: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={9}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   name="adress"
-//                   label="adress"
-//                   type="adress"
-//                   id="adress"
-//                   autoComplete="adress"
-//                   onChange={(e)=>updateForm({adress: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="password"
-//                   label="password"
-//                   name="password"
-//                   autoComplete="password"
-//                   onChange={(e)=>updateForm({password: e.target.value})}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <FormControlLabel
-//                   control={<Checkbox value="allowExtraEmails" color="primary" />}
-//                   label="I want to receive inspiration, marketing promotions and updates via email."
-//                 />
-//               </Grid>
-//             </Grid>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               sx={{ mt: 3, mb: 2 }}
-//               onClick={onSubmit}
-//             >
-//              <Link href="/clientSpace" variant="body2">
-//                   Sign Up
-//              </Link>
-//             </Button>
-//             <Grid container justifyContent="flex-end">
-//               <Grid item>
-//                 <Link href="/signin" variant="body2">
-//                   Already have an account? Sign in
-//                 </Link>
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Box>
-//       </Container>
-//     </ThemeProvider>
-        
-//     </div>
-//     )
-// }
-
-// export default ShoppingCart;
-
-
-
-
-
-
-
-import MenuItem from '@mui/material/MenuItem';
- import React, { useState } from "react";
- import { useNavigate } from "react-router";
-import FormHelperText from '@mui/material/FormHelperText';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
-const ShoppingCart=() =>{
-    let [cart, setsCart] = useState({
-        quantity:"",
-        product:"",
-        price:"",
-      
-      });
-      const navigate = useNavigate();
-  
-    //   function updateCart(value) {
-    //     return setsCart((prev) => {
-    //       return { ...prev, ...value };
-    //     });
-    //   }
-
-
- // This function will handle the submission.
- async function updateCart(e) {
-    e.preventDefault();
-  
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    const newCart = { ...cart };
-    console.log(newCart);
-  
-    await fetch("http://localhost:5000/cart", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newCart),
-    })
-    .catch(error => {
-      window.alert(error);
-      return;
-    });
-  
-    setsCart({ quantity: "", product: "", price: "", });
-    navigate("/");
-  };
-    
-
-
-  return (
-    <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }} error>
-        <InputLabel id="demo-simple-select-error-label">Quantity</InputLabel>
-        <Select
-          labelId="demo-simple-select-error-label"
-          id="demo-simple-select-error"
-          value={quantity}
-          label="Quantity"
-          onChange={(e)=>updateCart({quantity: e.target.value})} 
-          renderValue={(value) => `⚠️  - ${value}`}
-        >
-          <MenuItem value="">
-            <em>veuillez choisir</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>choisir</FormHelperText>
-      </FormControl>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-readonly-label">Product</InputLabel>
-        <Select
-          labelId="demo-simple-select-readonly-label"
-          id="demo-simple-select-readonly"
-          value={product}
-          label="Product"
-          onChange={(e)=>updateCart({product: e.target.value})} 
-        >
-          <MenuItem value="">
-            <em>veuillez choisir</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Read only</FormHelperText>
-      </FormControl>
-      <FormControl required sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-required-label">Price</InputLabel>
-        <Select
-          labelId="demo-simple-select-required-label"
-          id="demo-simple-select-required"
-          value={price}
-          label="Price *"
-          onChange={(e)=>updateCart({price: e.target.value})} 
-        >
-          <MenuItem value="">
-            <em>veuillez choisir</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Required</FormHelperText>
-      </FormControl>
+    return(
+    <div className='cart1'> 
+    {itemsData.map((item, index) => {
+    return(   
+    <div className='cart2'>
+    <p>{item.product}</p> <br/>
+    <p>{item.qty}</p><br/>
+    <p>{item.price}</p><br/>
     </div>
-  );
-}
+    )})
+    }
 
- export default ShoppingCart;
+    </div>
+    )}
+    export default ShoppingCart;
 
 
 
 
- // import React from 'react';
+
+
+
+
+//  import React from 'react';
 
 // const ShoppingCart = () => {
-//     return (
+
+//     [cart, setCart] = useState({
+//                 quantity:"",
+//                 product:"",
+//                 price:"",
+              
+//               });
+   
+   
+//               return (
 //         <div className='cart'>
 //             <div className='categories'>
 //                 <p>Quantity</p>
@@ -339,4 +75,4 @@ const ShoppingCart=() =>{
 //     )
 // }
 
-//export default ShoppingCart;
+// export default ShoppingCart;
